fa8e0780e42b2e0672617b2418353ec07d69e451
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-22 13:29:41 -0400

    Started index.js file within models folder

--- models/index.js
+++ models/index.js
@@ -1,2 +1,19 @@
-//index inside of models
-//insert code here
+const User = require('./User');
+const Books = require('./Books');
+const personalReadingList = require('./personalReadingList');
+const blog = require('./blog');
+
+User.hasMany(Books, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Books.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+personalReadingList.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, personalReadingList };
