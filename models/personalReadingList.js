0426d4c4bd03880f3b4b1b71ebc41aef3f17b6b0
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-05-22 09:30:06 -0500

    personal reading list table

---
+++ models/personalReadingList.js
@@ -0,0 +1,43 @@
+//Personal Reading List Table
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class prList extends Model { }
+
+prList.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        book_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        book_author: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        book_description: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'prList',
+    }
+);
+
+module.exports = prList;
