6465d77df83488b3ced88054aeb7b21017b12a59
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-24 19:11:00 -0400

    Updated index.js file within models folder

--- models/index.js
+++ models/index.js
@@ -3,17 +3,18 @@ const Books = require('./Books');
 const personalReadingList = require('./personalReadingList');
 const blog = require('./blog');
 
-User.hasMany(Books, {
+// blog.hasMany(User, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+User.hasOne(personalReadingList, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-});
-
-Books.belongsTo(User, {
-    foreignKey: 'user_id'
 });
 
 personalReadingList.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, personalReadingList };
+module.exports = { User, personalReadingList, Books, blog };
