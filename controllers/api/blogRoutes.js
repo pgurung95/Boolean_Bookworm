e34a415c6d61fe6c33ac7f389874de959c043ad1
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-24 21:31:26 -0400

    Added Routes files

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,3 @@
+// router.post when a new blog comment is created
+// router.put for updating comments
+// router.delete for deleting blog comments
---
+++ controllers/api/prListRoutes.js
@@ -0,0 +1,4 @@
+// router.get current list
+// router.put update current list
+// router.post add book to reading list
+// router.delete to delete book from reading list
