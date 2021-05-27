c09426734883870b104ae42bf388d10dbc28199c
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-26 20:21:59 -0400

    Worked on homeRoutes.js file, populated login.js and logout.js files within js/public folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,3 +7,71 @@
 // get = /booleanbookworm (takes user to logged-in "homepage" that displays blog - from link in the users profile)
 
 // get = /login (takes user to login/sign-up page)
+
+const router = require('express').Router();
+const { Books, personalReadingList } = require('../models');
+// Import the custom middleware
+// const withAuth = require('../utils/auth');
+
+// GET all previous read books from book club for homepage
+router.get('/', async (req, res) => {
+    try {
+        const booksData = await Books.findAll({
+            include: [
+                {
+                    model: Books,
+                    attributes: ['title']
+                },
+            ],
+        });
+
+        const books = booksData.map((book) =>
+            book.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            books,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET profile
+router.get('/profile', async (req, res) => {
+    try {
+        const prBooksData = await personalReadingList.findAll({
+            include: [
+                {
+                    model: Books,
+                    attributes: ['title'],
+                },
+            ],
+        });
+
+        const books = booksData.map((book) =>
+            book.get({ plain: true })
+        );
+
+        res.render('profile', {
+            books,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
