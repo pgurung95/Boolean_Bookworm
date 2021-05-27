0877dd1f4e90476ae632fe5bc991058fc5129b5e
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-05-26 20:27:49 -0400

    created blog routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,20 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+// //const userRoutes = require('./userRoutes');
 
-const userRoutes = require('./user-routes');
+// //const prListRoutes = require('./prListRoutes');
+
+// //router.use('/users', userRoutes);
+
+// //router.use('/prList', prListRoutes);
+
+// module.exports = router;
+
+const router = require('express').Router();
+const blogRoutes = require('./blogRoutes');
+
+const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
+router.use('/blog', blogRoutes);
 
 module.exports = router;
