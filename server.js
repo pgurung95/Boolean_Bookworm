2e8671e58ab52621cb9c2267908a93c2577c9b9a
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-26 21:37:35 -0400

    Updates from end of class

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session(sess));
 //const hbs = exphbs.create({ helpers });
 const hbs = exphbs.create();
 
-//app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
